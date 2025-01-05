5afcb179cd318279a360b8d5a1f26cc82751644f
rupalisinghjava@gmail.com <rupalisinghjava920@gmail.com>
2025-01-05 13:04:46 -0800

    product service handle exception custome

---
+++ bhavna-cops/src/main/java/product/service/unit/Constant.java
@@ -0,0 +1,15 @@
+package product.service.unit;
+
+public class Constant {
+
+   public static final String SAVE_DATA="Data successfully save";
+    public static final String PRODUCT_NOT_SAVE="Product is not save";
+    public static final String PRODUCT_ID="Product with id is not found.";
+    public static final String PRODUCT_GET="An error occurred while getting the product";
+    public static final String PRODUCT_UPDATE="An error occurred while updating the product";
+    public static final String DELETE_ID="Product deleted successfully";
+    public static final String PRODUCT_DELETE="An error occurred while deleting the product";
+
+    public static final String PRODUCT_NOT_FOUND="No products found with the name: ";
+    public static final String PRODUCT_NAME_RELATED="An error occurred while retrieving products by name: ";
+}
